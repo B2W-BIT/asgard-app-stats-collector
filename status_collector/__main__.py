fb1f6a23e28337d0265ac934f8eb0a7722a3f7da
Dalton Barreto <daltonmatos@gmail.com>
2018-07-12 20:33:20 -0300

    Teste do fluxo completo, com slave dando exception
    Confere que mesmo que um slave retorne exception, os outros geram
    dados que são de fato enviados para a fila

--- status_collector/__main__.py
+++ status_collector/__main__.py
@@ -1,9 +1,8 @@
 import asyncio
 from status_collector import conf
 from easyqueue.async import AsyncQueue
-from status_collector import Test, get_slave_ip_list, get_slave_statistics, send_slave_statistics_to_queue, async_tasks
+from status_collector import Test, get_slave_ip_list, get_slave_statistics, send_slave_statistics_to_queue, async_tasks, fetch_app_stats
 from aiologger.loggers.json import JsonLogger
-
 
 async def main():
     logger = await JsonLogger.with_default_handlers(level=10, flatten=True)
@@ -13,14 +12,7 @@ async def main():
         conf.STATUS_COLLECTOR_RABBITMQ_PWD,
         delegate=Test(),
         virtual_host=conf.STATUS_COLLECTOR_RABBITMQ_VHOST)
-    ip_list = await get_slave_ip_list(conf.STATUS_COLLECTOR_MESOS_MASTER_IP)
-    await logger.debug({"totalSlaves": len(ip_list), "slaveList": ip_list})
-    
-    try:
-        tasks = [async_tasks(ip, logger, queue) for ip in ip_list]
-        await asyncio.gather(*tasks)
-    except Exception as e:
-        await logger.exception(e)
+    await fetch_app_stats(queue, logger)
 
 
 asyncio.get_event_loop().run_until_complete(main())
