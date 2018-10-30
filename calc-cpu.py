265562188cad3b7397c8d964c8b6abec07229586
Dalton Barreto <daltonmatos@gmail.com>
2018-10-30 16:42:22 -0300

    Prova de conceito para cálculo de uso de CPU
    Apenas um código simples para me familiarizar com os números que o Mesos
    expõe.
    
    IN-2641

---
+++ calc-cpu.py
@@ -0,0 +1,45 @@
+import asyncio
+import aiohttp
+
+import os
+
+agent_ip = os.environ.get("AGENT_IP")
+
+
+CACHE = {}
+
+
+async def main():
+    while True:
+        s = aiohttp.ClientSession()
+        r = await s.get(f"http://{agent_ip}:5051/monitor/statistics.json")
+        data = await r.json()
+        await s.close()
+        for task_data in data:
+            task_name = task_data['source']
+            if task_name not in CACHE:
+                CACHE[task_name] = task_data
+                continue
+            before = CACHE[task_name]
+            now = task_data
+            delta_t = now['statistics']['timestamp'] - before['statistics']['timestamp']
+            cpu_usr_secs = now['statistics']['cpus_user_time_secs'] - before['statistics']['cpus_user_time_secs']
+            cpu_sys_secs = now['statistics']['cpus_system_time_secs'] - before['statistics']['cpus_system_time_secs']
+            cpu_thr_secs = now['statistics']['cpus_throttled_time_secs'] - before['statistics']['cpus_throttled_time_secs']
+
+
+            cpu_usr_pct = cpu_usr_secs / delta_t * 100
+            cpu_thr_pct = cpu_thr_secs / delta_t * 100
+            cpu_usr_pct_mesos = cpu_usr_pct / 100 / now['statistics']['cpus_limit'] * 100
+
+            print (f"{task_name} elapsed={delta_t:.3f}, \
+usr_secs={cpu_usr_secs:.3f}, usr_pct={cpu_usr_pct:.3f}%, usr_pct_meso={cpu_usr_pct_mesos:.3f}% ({cpu_usr_pct/100}, {now['statistics']['cpus_limit']}), \
+thr_secs={cpu_thr_secs:.3f}, thr_pct={cpu_thr_pct:.3f}%,\
+")
+            CACHE[task_name] = now
+
+        print ("")
+        await asyncio.sleep(1)
+
+
+asyncio.get_event_loop().run_until_complete(main())
