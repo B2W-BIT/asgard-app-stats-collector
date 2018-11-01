bd161caf8c98356e8dfe6149c24345cd9d25fd5d
Dalton Barreto <daltonmatos@gmail.com>
2018-11-01 16:49:46 -0300

    Removendo testes antigos
    IN-2641

--- tests/test_fetch_master.py
+++ tests/test_fetch_master.py
@@ -1,3 +1,5 @@
+import asyncio
+from asynctest import mock
 import asynctest
 from status_collector import get_slave_ip_list
 from status_collector import get_slave_statistics
@@ -74,25 +76,6 @@ slave_statistics_response_mock = [{
     }
 }]
 
-slave_statistics_response_mock_without_all_fields = [{
-    "executor_id":
-    "sieve_captura_kirby_powerup.947f6124-762a-11e8-b78b-c678b0156430",
-    "executor_name":
-    "Command Executor (Task: sieve_captura_kirby_powerup.947f6124-762a-11e8-b78b-c678b0156430) (Command: NO EXECUTABLE)",
-    "framework_id":
-    "27b52920-3899-4b90-a1d6-bf83a87f3612-0000",
-    "source":
-    "sieve_captura_kirby_powerup.947f6124-762a-11e8-b78b-c678b0156430",
-    "statistics": {
-        "cpus_limit": 8.25,
-        "cpus_system_time_secs": 34501.45,
-        "cpus_user_time_secs": 96348.84,
-        "mem_limit_bytes": 7650410496,
-        "mem_rss_bytes": 5105614848,
-        "timestamp": 1388534400.0
-    }
-}]
-
 slave_statistics_response_mock_multiple_tasks = [{
     "executor_id":
     "sieve_captura_kirby_powerup.947f6124-762a-11e8-b78b-c678b0156430",
@@ -154,155 +137,6 @@ class FecthMasterTest(asynctest.TestCase):
             slave_ips,
             ["10.0.111.32:5051", "10.0.111.33:5051", "10.0.111.34:5051"])
 
-    async def test_slave_exists_with_all_fields(self):
-        with aioresponses() as m:
-            m.get(
-                'http://10.0.111.32:5051/monitor/statistics.json',
-                payload=slave_statistics_response_mock)
-            slave_statistics = await get_slave_statistics(
-                "10.0.111.32:5051", self.loggerMock)
-            expected_result = [{
-                "appname":
-                "/sieve/captura/kirby/powerup",
-                "task_name":
-                "sieve_captura_kirby_powerup.947f6124-762a-11e8-b78b-c678b0156430",
-                "cpu_throttled_percentage":
-                0.26874181769336525,
-                "memory_usage_percentage":
-                66.73648231907895,
-                "cpus_limit":
-                8.25,
-                "cpus_nr_periods":
-                769021,
-                "cpus_nr_throttled":
-                1046,
-                "cpus_system_time_secs":
-                34501.45,
-                "cpus_throttled_time_secs":
-                352.597023453,
-                "cpus_user_time_secs":
-                96348.84,
-                "mem_anon_bytes":
-                4845449216,
-                "mem_file_bytes":
-                260165632,
-                "mem_limit_bytes":
-                7650410496,
-                "mem_mapped_file_bytes":
-                7159808,
-                "mem_rss_bytes":
-                5105614848,
-                "timestamp":
-                1388534400.0
-            }]
-            self.assertEquals(slave_statistics, expected_result)
-
-    async def test_slave_exists_without_all_fields(self):
-        with aioresponses() as m:
-            m.get(
-                'http://10.0.111.32:5051/monitor/statistics.json',
-                payload=slave_statistics_response_mock_without_all_fields)
-            slave_statistics = await get_slave_statistics(
-                "10.0.111.32:5051", self.loggerMock)
-            expected_result = [{
-                "appname":
-                "/sieve/captura/kirby/powerup",
-                "task_name":
-                "sieve_captura_kirby_powerup.947f6124-762a-11e8-b78b-c678b0156430",
-                "memory_usage_percentage":
-                66.73648231907895,
-                "cpus_limit":
-                8.25,
-                "cpus_system_time_secs":
-                34501.45,
-                "cpus_user_time_secs":
-                96348.84,
-                "mem_limit_bytes":
-                7650410496,
-                "mem_rss_bytes":
-                5105614848,
-                "timestamp":
-                1388534400.0
-            }]
-            self.assertEquals(slave_statistics, expected_result)
-
-    async def test_get_slave_returns_list_of_tasks(self):
-        self.maxDiff = None
-        with aioresponses() as m:
-            m.get(
-                'http://10.0.111.32:5051/monitor/statistics.json',
-                payload=slave_statistics_response_mock_multiple_tasks)
-            slave_statistics = await get_slave_statistics(
-                "10.0.111.32:5051", self.loggerMock)
-            expected_result = [{
-                "appname":
-                "/sieve/captura/kirby/powerup",
-                "task_name":
-                "sieve_captura_kirby_powerup.947f6124-762a-11e8-b78b-c678b0156430",
-                "cpu_throttled_percentage":
-                0.26874181769336525,
-                "memory_usage_percentage":
-                66.73648231907895,
-                "cpus_limit":
-                8.25,
-                "cpus_nr_periods":
-                769021,
-                "cpus_nr_throttled":
-                1046,
-                "cpus_system_time_secs":
-                34501.45,
-                "cpus_throttled_time_secs":
-                352.597023453,
-                "cpus_user_time_secs":
-                96348.84,
-                "mem_anon_bytes":
-                4845449216,
-                "mem_file_bytes":
-                260165632,
-                "mem_limit_bytes":
-                7650410496,
-                "mem_mapped_file_bytes":
-                7159808,
-                "mem_rss_bytes":
-                5105614848,
-                "timestamp":
-                1388534400.0
-            }, {
-                "appname":
-                "/sieve2/captura/kirby/powerup",
-                "task_name":
-                "sieve2_captura_kirby_powerup.947f6124-762a-11e8-b78b-c678b0156430",
-                "cpu_throttled_percentage":
-                0.26874181769336525,
-                "memory_usage_percentage":
-                66.73648231907895,
-                "cpus_limit":
-                8.25,
-                "cpus_nr_periods":
-                769021,
-                "cpus_nr_throttled":
-                1046,
-                "cpus_system_time_secs":
-                34501.45,
-                "cpus_throttled_time_secs":
-                352.597023453,
-                "cpus_user_time_secs":
-                96348.84,
-                "mem_anon_bytes":
-                4845449216,
-                "mem_file_bytes":
-                260165632,
-                "mem_limit_bytes":
-                7650410496,
-                "mem_mapped_file_bytes":
-                7159808,
-                "mem_rss_bytes":
-                5105614848,
-                "timestamp":
-                1388534400.0
-            }]
-            self.assertEquals(slave_statistics, expected_result)
-
     async def test_slave_dont_exists(self):
         with aioresponses() as m:
             with self.assertRaises(Exception):
@@ -322,7 +156,6 @@ class FecthMasterTest(asynctest.TestCase):
                     "10.0.1.1:5051")
 
     async def test_putting_slave_statistics_on_rabbitMQ(self):
-        self.maxDiff = None
         with aioresponses() as m:
             m.get('http://10.11.43.96:5050/slaves', payload=slaves)
             slave_ips = await get_slave_ip_list("10.11.43.96")
@@ -339,7 +172,7 @@ class FecthMasterTest(asynctest.TestCase):
         self.assertEquals([
             asynctest.mock.call(
                 body=slave_statistics[0],
-                routing_key=conf.STATUS_COLLECTOR_RABBITMQ_RK)
+                routing_key=conf.STATS_COLLECTOR_RABBITMQ_RK)
         ], queue.put.await_args_list)
 
     async def test_putting_slave_multiple_tasks_statistics_on_rabbitMQ(self):
@@ -360,39 +193,8 @@ class FecthMasterTest(asynctest.TestCase):
         self.assertEquals([
             asynctest.mock.call(
                 body=slave_statistics[0],
-                routing_key=conf.STATUS_COLLECTOR_RABBITMQ_RK),
+                routing_key=conf.STATS_COLLECTOR_RABBITMQ_RK),
             asynctest.mock.call(
                 body=slave_statistics[1],
-                routing_key=conf.STATUS_COLLECTOR_RABBITMQ_RK)
+                routing_key=conf.STATS_COLLECTOR_RABBITMQ_RK)
         ], queue.put.await_args_list)
-
-    async def test_get_statistic_on_slave_an_put_into_queue(self):
-        self.maxDiff = None
-        with aioresponses() as m:
-            m.get('http://10.11.43.96:5050/slaves', payload=slaves)
-
-            m.get( f'http://{slaves["slaves"][0]["hostname"]}:5051/monitor/statistics.json', payload=slave_statistics_response_mock_multiple_tasks)
-            m.get( f'http://{slaves["slaves"][1]["hostname"]}:5051/monitor/statistics.json', exception=Exception())
-            m.get( f'http://{slaves["slaves"][2]["hostname"]}:5051/monitor/statistics.json', payload=slave_statistics_response_mock_multiple_tasks)
-
-            queue = asynctest.mock.CoroutineMock(put=asynctest.mock.CoroutineMock(), connect=asynctest.mock.CoroutineMock())
-            slave_statistics = list(map(build_statistic_for_response, slave_statistics_response_mock_multiple_tasks))
-
-            with asynctest.mock.patch.object(conf, "STATUS_COLLECTOR_MESOS_MASTER_IP", "10.11.43.96"):
-                await fetch_app_stats(queue, self.loggerMock)
-
-                self.assertEqual(4, queue.put.await_count)
-                self.assertEquals([
-                    asynctest.mock.call(
-                        body=slave_statistics[0],
-                        routing_key=conf.STATUS_COLLECTOR_RABBITMQ_RK),
-                    asynctest.mock.call(
-                        body=slave_statistics[1],
-                        routing_key=conf.STATUS_COLLECTOR_RABBITMQ_RK),
-                    asynctest.mock.call(
-                        body=slave_statistics[0],
-                        routing_key=conf.STATUS_COLLECTOR_RABBITMQ_RK),
-                    asynctest.mock.call(
-                        body=slave_statistics[1],
-                        routing_key=conf.STATUS_COLLECTOR_RABBITMQ_RK)
-                ], queue.put.await_args_list)
